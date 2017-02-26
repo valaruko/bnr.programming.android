c3c4bce98b634b41fe81b574f7398c00bd8e35a7
Daniel Brubeck <daniel.brubeck@gmail.com>
2017-02-26 10:13:39 -0500

    Crime Fragment
    The crime activity now starts the new CrimeFragment fragment that will
    start the basic new crime manager, for now it only set's the crime's title

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,13 +1,26 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
